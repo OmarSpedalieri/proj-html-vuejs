330587dd0bff3e1abc7ee12f4756f9b99a10c82b
OmarSpedalieri <omarspedalieri.vfx@gmail.com>
2020-12-09 17:17:37 +0100

    stating layout

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// var app = new Vue({
+//
+// el:"#nav_links",
+// data:{
+//     links:[
+//       "HOME",
+//       "ABOUT",
+//       "SERVICES",
+//       "PROCESS",
+//       "TESTIMONIALS"
+//     ],
+//     logo:"img/fake_logo.png"
+// },
+//   methods:{
+//
+//
+//   }
+//
+//
+// })
