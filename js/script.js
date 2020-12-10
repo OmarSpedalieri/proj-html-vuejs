0b4962d711b7ef5356968f13e8b14ddeb2367f9f
OmarSpedalieri <omarspedalieri.vfx@gmail.com>
2020-12-10 12:20:36 +0100

    Added footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var app = new Vue({
+var nav = new Vue({
 
 el:"#nav_links",
 data:{
@@ -9,12 +9,64 @@ data:{
       "PROCESS",
       "TESTIMONIALS"
     ],
-    logo:"img/fake_logo.png"
+    logo:"img/fake_logo.png",
   },
 methods:{
 
 
   }
 
+});
+
+
+var foot = new Vue({
+
+el:"#foot_links",
+data:{
+    foot_index:0,
+    footer_links:[
+    {
+      links:
+      [
+      "The Company",
+      "Institutional",
+      "Social & Events",
+      "Innovation",
+      "Environment",
+      "Technology"
+      ]
+    },
+
+    {
+      links:
+      [
+      "Industrialized",
+      "chemicals",
+      "packaged Liquids",
+      "Construction",
+      "Laminated Xood",
+      "And Others"
+      ]
+    },
+
+    {
+      links:
+      [
+      "Responsibility",
+      "Term of Use",
+      "About Cookies",
+      "Privacy Policy",
+      "Accessibility",
+      "Information"
+      ]
+    }
+
+    ]
+
+  },
+methods:{
+
+
+  }
 
 })
